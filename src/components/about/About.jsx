b75dc817170e01277ef5d281f6a84f032baf45eb
Ranjan Behl <behlranjan@gmail.com>
2022-05-25 21:24:13 -0400

    skeleton code is working

---
+++ src/components/about/About.jsx
@@ -0,0 +1,35 @@
+import React from 'react'
+import './about.css'
+import ME from '../../assets/me.jpg'
+import {FaAward} from 'react-icons/fa'
+
+const About = () => {
+  return (
+    <section id='about'>
+      <h5>Get To Know</h5>
+      <h2>About Me</h2>
+
+      <div className='container about__container'>
+          <div className='about__me'>
+            <div className='about__me-image'>
+              <img src={ME} alt='About'/>
+            </div>
+          </div>
+          
+          <div className='about__content'>
+              {/* <div className='about__cards'>
+                <article className='about__card'>
+                  <FaAward className='about__icon'/>
+                  <h5>Experience</h5>
+                  <small>Years Working</small>
+                </article>
+              </div> */}
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Suscipit deleniti vitae nihil impedit mollitia modi aliquid ex totam ipsum laborum ratione hic temporibus consectetur, pariatur animi. Adipisci, error. Esse, quia.</p>
+            <a href='#contact' className='btn btn-primary'>Let's Talk</a>
+          </div>
+      </div>
+    </section>
+  )
+}
+
+export default About
---
+++ src/components/contact/Contact.jsx
@@ -0,0 +1,34 @@
+import React from 'react'
+import './contact.css'
+import { useRef } from 'react';
+import emailjs from 'emailjs-com'
+
+const Contact = () => {
+  const form = useRef();
+   
+    const sendEmail = (e) => {
+      e.preventDefault();
+  
+      emailjs.sendForm('service_zyw832n', 'template_no7dc1q', form.current, 'hMuJqhkc8syei1V1K')
+       
+      e.target.reset()
+    };
+  
+  return (
+    <section id='contact'>
+      <h5>Get In Touch</h5>
+      <h2>Contact Me</h2>
+      
+      <div className='container contact__container'>
+        <form ref={form} onSubmit = {sendEmail}>
+          <input type='text' name='name' placeholder='Your Full Name' required/>
+          <input type='email' name='email' placeholder='Your Email' required />
+          <textarea name='message' rows='7' placeholder='Your Message' required></textarea>
+          <button type='submit' className='btn btn-primary'>Send Message</button>
+        </form>
+      </div>
+    </section>
+  )
+}
+
+export default Contact
